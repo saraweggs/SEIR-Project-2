ea78f84570f8f4a7543180a16f59a62dee0c99bf
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-05-02 15:31:16 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+require('dotenv').config();
+
+// CONFIGURATION
+const PORT = process.env.PORT || 3000;
+const mongoURI = process.env.MONGODB_URI
 
 
 // MIDDLEWARE
@@ -10,18 +15,18 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
+// DATABASE
+mongoose.connect(mongoURI, {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+  console.log('connected to mongo');
+})
+
 // CONTROLLER
 const productsController = require('./controllers/products.js');
 app.use('/ootd', productsController);
 
-// LISTENER
-app.listen(3000, () => {
-  console.log('listening...');
-})
 
-// DATABASE CONNECTION 
-mongoose.connect('mongodb://localhost:27017/ootdapp', {useNewUrlParser: true});
-mongoose.connection.once('open', () => {
-  console.log('connected to mongo');
+// LISTENER
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
 })
-const db = mongoose.connection
