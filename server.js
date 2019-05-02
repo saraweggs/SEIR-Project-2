c16a79a7e66cc595ee692722d9076e522e3ec7f9
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-05-02 15:33:40 -0400

    removed env config from server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-require('dotenv').config();
 
 // CONFIGURATION
 const PORT = process.env.PORT || 3000;
