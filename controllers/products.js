a4f58ffb158595f08e3dfeb19018e4ab1111f80e
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-05-02 15:09:15 -0400

    all routes work

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,60 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/product.js');
+
+// NEW ROUTE
+router.get('/new', (req, res) => {
+  res.render('new.ejs')
+})
+
+
+// INDEX ROUTE
+router.get('/', (req, res) => {
+  Product.find({}, (error, allProducts) => {
+    res.render('index.ejs', {
+      products: allProducts
+    })
+  })
+})
+
+// DELETE ROUTE
+router.delete('/:id', (req, res) => {
+  Product.findByIdAndRemove(req.params.id, (error, deleteProduct) => {
+    res.redirect('/ootd')
+  })
+})
+
+router.put('/:id', (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  }, (error, updatedModel) => {
+    res.redirect('/ootd/' + req.params.id)
+  })
+})
+
+// EDIT ROUTE
+router.get('/:id/edit', (req, res) => {
+  Product.findById(req.params.id, (error, editProduct) => {
+    res.render('edit.ejs', {
+      products: editProduct
+    })
+  })
+})
+
+// SHOW ROUTE
+router.get('/:id', (req, res) => {
+  Product.findById(req.params.id, (error, showProduct) => {
+    res.render('show.ejs', {
+      products: showProduct
+    })
+  })
+})
+
+// CREATE ROUTE
+router.post('/', (req, res) => {
+  Product.create(req.body, (error, createProduct) => {
+    res.redirect('/ootd')
+  })
+})
+
+module.exports = router;
--- models/product.js
+++ models/product.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   name: {type: String, required: true},
-  description: String,
+  season: {type: String, required: true},
+  description: String,   // GOAL: turn to array to display as bullet points on show page
   img: String,
   price: {type: Number, min: 0},
   link: String,
