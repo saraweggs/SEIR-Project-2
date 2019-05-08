0803ce3f6bb7a7a2f7ba52d0c75cab58ca26a404
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-05-07 21:56:39 -0400

    worked on responsiveness

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,23 +1,20 @@
-const express= require('express');
+const express = require('express');
 const sessions = express.Router();
 const User = require('../models/users.js');
 
 
-// sessions.get('/new', (req, res) => {
-//   res.render('sessions/new.ejs', {
-//     currentUser: req.session.currentUser
-//   })
-// })
 
-sessions.post('/', (req, res)=>{
-    User.findOne({ username: req.body.username }, (err, foundUser) => {
-        if(req.body.password == foundUser.password){
-          req.session.currentUser = foundUser
-            res.redirect('/ootd')
-        } else {
-          res.send('<a href="/">wrong password</a>')
-        }
-    });
+sessions.post('/', (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }, (err, foundUser) => {
+    if (req.body.password == foundUser.password) {
+      req.session.currentUser = foundUser
+      res.redirect('/ootd')
+    } else {
+      res.send('<a href="/">wrong password</a>')
+    }
+  });
 });
 
 sessions.delete('/', (req, res) => {
--- public/app.js
+++ public/app.js
@@ -1,26 +1,23 @@
 $(() => {
 
-// ELEMENTS
-const $openBtn = $('#openModal');
-const $modal = $('#modal');
-const $closeBtn = $('#close');
+  // ELEMENTS
+  const $openBtn = $('#openModal');
+  const $modal = $('#modal');
+  const $closeBtn = $('#close');
 
 
-// EVENT HANDLERS
-const openModal = () => {
-  $modal.css('display', 'block');
-}
+  // EVENT HANDLERS
+  const openModal = () => {
+    $modal.css('display', 'block');
+  }
 
-const closeModal = () => {
-  $modal.css('display', 'none');
-}
+  const closeModal = () => {
+    $modal.css('display', 'none');
+  }
 
-const collectItem = () => {
 
-}
-
-// EVENT LISTENER
-$openBtn.on('click', openModal);
-$closeBtn.on('click', closeModal)
+  // EVENT LISTENER
+  $openBtn.on('click', openModal);
+  $closeBtn.on('click', closeModal)
 
 })
